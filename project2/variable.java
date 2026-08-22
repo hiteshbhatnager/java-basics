a455fda9d109733ff8f13f09c2b7b690d3c74462
hitesh <bhatnagerhitesh@gmail.com>
2026-08-22 19:08:45 +0530

    java first

---
+++ project2/variable.java
@@ -0,0 +1,13 @@
+public class variable {
+    public static void main(String[] args) {
+        int value = 18;
+        double mark = 88.34;
+        char abc = 'a';
+        boolean hello = false;
+
+        System.out.println(value);
+        System.out.println(mark);
+        System.out.println(abc);
+        System.out.println(hello);
+    }
+}
---
+++ project3/operater.java
@@ -0,0 +1,20 @@
+
+public class operater {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 2.4;
+        int three = 5;
+
+        double sum = a + b;
+        double minus = a - b;
+        double multipuly = a * b;
+        double divide = a / b;
+        int module = a % three;
+
+        System.out.println(sum);
+        System.out.println(minus);
+        System.out.println(multipuly);
+        System.out.println(divide);
+        System.out.println(module);
+    }
+}
