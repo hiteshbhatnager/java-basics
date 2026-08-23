a1728550957e4b3db7a7a92ec7f5b33382d9bbf3
hitesh <bhatnagerhitesh@gmail.com>
2026-08-23 16:22:04 +0530

    input

---
+++ project6/input.java
@@ -0,0 +1,14 @@
+package project6;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter you name");
+
+        String name = sc.next();
+        System.out.println("hello" + name);
+    }1
+}
