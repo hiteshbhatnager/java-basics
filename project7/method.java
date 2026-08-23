c6a97a0cf42b8796684bdbb75dbad5bd5e0faab3
hitesh <bhatnagerhitesh@gmail.com>
2026-08-23 16:21:27 +0530

    input

---
+++ project7/method.java
@@ -0,0 +1,18 @@
+package project7;
+
+import java.util.Scanner;
+
+public class method {
+
+    public static void welcome(String name) {
+        System.out.println("welcome" + name);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        welcome(name);
+    }
+}
