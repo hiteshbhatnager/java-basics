0147678efe8e40b9c5ddeb29ca959c0445c9432c
hitesh <bhatnagerhitesh@gmail.com>
2026-08-23 17:10:00 +0530

    array

---
+++ project8/array.java
@@ -0,0 +1,21 @@
+package project8;
+
+public class array {
+    public static void main(String[] args) {
+        int[] marks = { 10, 20, 30, 40, 50 };
+
+        // System.out.println(marks[2]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[0]);
+
+        // marks[4] = 1000;
+        // System.out.println(marks[4]);
+
+        // System.out.println(marks.length);
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+
+    }
+}
