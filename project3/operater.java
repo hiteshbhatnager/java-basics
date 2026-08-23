5d07044f1827a49ebe279e8788c2ca58d3a0d87c
hitesh <bhatnagerhitesh@gmail.com>
2026-08-23 11:20:37 +0530

    part 3 operater

--- project3/operater.java
+++ project3/operater.java
@@ -11,10 +11,29 @@ public class operater {
         double divide = a / b;
         int module = a % three;
 
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+
         System.out.println(sum);
         System.out.println(minus);
         System.out.println(multipuly);
         System.out.println(divide);
         System.out.println(module);
+
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(true || true);
+        System.out.println(true || false);
+
+        int x = 10;
+        x++;
+        System.err.println(x);
+        int y = 20;
+        ++y;
+        System.out.println(y);
+        int z = 10;
+        z += x;
+        System.out.println(z);
     }
 }
